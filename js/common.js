eeb9fad826d120eba1674d12dfc3cae5fa93bf1b
Ecat <qule520@126.com>
2017-02-26 19:33:12 +0800

    上传日语js库文件

--- js/common.js
+++ js/common.js
@@ -6,24 +6,8 @@ var HJ={
 	changeLanguage:function(){}
 }
 }
-//处理请求结果并展示到卡片中
-function wrapContent(content){
-	var trans = document.getElementById('trans')
-	trans.innerHTML=content
-	var audioSpans=document.querySelectorAll('span[title=日语发音]')
- //处理音频链接
- for(var i=0;i<audioSpans.length;i++){
- 	var audioSrc=audioSpans[i].lastChild.innerText
- 	audioSpans[i].innerHTML="<img class='playbutton' onclick='playAudio(\""+audioSrc+"\")' src='http://ecator.github.io/anki-theme-hjbasic/img/sound.gif'>"
- }
-}
-//向沪江请求单词释义
-function getTrans(){
-	var word=document.getElementById('word').innerText
-	var script=document.createElement('script')
-	script.setAttribute('src','http://dict.hjenglish.com/services/huaci/jp_web_ajax.ashx?type=jc&w='+encodeURI(word))
-	document.body.appendChild(script)
-}
+
+
 //播放音频
 function playAudio(audioSrc) {
 // alert(audioSrc)
---
+++ js/wrapContent_jp.js
@@ -0,0 +1,12 @@
+//处理请求结果并展示到卡片中
+//本方法处理日语结果
+function wrapContent(content){
+	var trans = document.getElementById('trans')
+	trans.innerHTML=content
+	var audioSpans=document.querySelectorAll('span[title=日语发音]')
+ //处理音频链接
+ for(var i=0;i<audioSpans.length;i++){
+ 	var audioSrc=audioSpans[i].lastChild.innerText
+ 	audioSpans[i].innerHTML="<img class='playbutton' onclick='playAudio(\""+audioSrc+"\")' src='http://ecator.github.io/anki-theme-hjbasic/img/sound.gif'>"
+ }
+}
