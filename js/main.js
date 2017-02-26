b13c18bc09b9f0318426daa3b028096cbd7f3613
Ecat <qule520@126.com>
2017-02-27 01:06:25 +0800

    自己实现define

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+//定义define函数
+function define(){
+	if(typeof(arguments[0])=='object'){
+		// console.log(arguments[0])
+		for(var dep in arguments[0]){
+			loadjs(arguments[0][dep])
+		}
+	}else if (typeof(arguments[0]=='function')) {
+		console.log(arguments[0])
+	}
+}
+function loadjs(filename){
+	var script=document.createElement("script")
+	script.setAttribute("src",filename+'.js')
+	document.head.appendChild(script)
+}
 //语言和加载的js文件映射
 var lanMap=[]
 lanMap['jp']="jp"
